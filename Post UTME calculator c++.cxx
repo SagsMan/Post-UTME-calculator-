63019bff2aaf19b07c9f09e3d550630a5151cb18
Sagiru Garba <122167371+SagsMan@users.noreply.github.com>
2024-07-19 18:13:24 +0100

    Add files via upload
    SagsMan aim to develop this software using C++ to ease POST UTME score calculation for newly admitted students

---
+++ Post UTME calculator c++.cxx
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+int post_utme_score;
+cout<<"Enter post utme score::- ";
+cin>>post_utme_score;
+
+if (post_utme_score >=40)
+{cout<<"you'll: get Admission";
+
+
+} 
+else if(post_utme_score >=30)
+{cout<<"You'll be given Admission but your course of study will be CHANGED!";}
+else
+{cout<<"probably or NOT you might get Admission but your score percentage is LOW";
+}
+
+return 0; } 
